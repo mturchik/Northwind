8b6069f406ff2f0e01d893e7a96235d4b44fa91a
mturchik <mturchik@my.wctc.edu>
2019-10-18 13:51:26 -0500

    Added Customer Migration

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Northwind.Models;
+
+namespace Northwind.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly INorthwindRepository _db;
+
+        public CustomerController(INorthwindRepository db)
+        {
+            _db = db;
+        }
+        
+        public IActionResult Register() => View();
+    }
+}
